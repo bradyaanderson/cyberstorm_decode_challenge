425ca08b99d5df35bc7bc28b20e87dec0a1e8d83
bradyaanderson <bradya48@gmail.com>
2017-04-22 14:13:11 -0500

    skeleton for project

--- main.py
+++ main.py
@@ -8,3 +8,36 @@
 from Tkinter import *
 # import RPi.GPIO as GPIO
 import codecs
+
+# encodes a string given a certain value
+def encode(string, code):
+	pass
+
+# decodes a string given a certain value
+def decode(string, code):
+	pass
+
+# the answer will be equal to some string value
+def get_answer():
+	return "some string"
+
+
+
+# main
+solved = False
+answer = get_answer()
+code = "some random value"
+
+# encodes the message as some value
+encode_value = encode(answer, code)
+
+# continues until solved
+while not solved:
+
+	guess_value = "something"
+
+	# when correct code is input into decode, correct value will be returned
+	decode_value = decode(guess_value, code)
+
+	if decode_value == encode_value:
+		not_solved = True
