1e55b28739a81baaba8c3178554acb6ecbada8b3
bradyaanderson <bradya48@gmail.com>
2017-05-04 13:04:45 -0500

    add gui

--- main.py
+++ main.py
@@ -6,9 +6,158 @@
 # to get the correct name of a constellation.
 ############################
 import RPi.GPIO as GPIO
-import time
+from Tkinter import *
+from time import time
+from datetime import timedelta
 
-class Cipher():
+
+
+
+# creates gui
+class App:
+    def __init__(self):
+        # gui set-up
+        self.WIDTH = 1280
+        self.HEIGHT = 720
+        self.root = Tk()
+        self.root.title("Constellation Crack")
+        self.root.configure(bg="black")
+        self.root.geometry('{}x{}'.format(self.WIDTH, self.HEIGHT))
+        self.set_up_gui()
+
+        # create timer object
+        self.game_time = Timer(300)
+        self.update_timer()
+
+        # creates challenge and game object
+        self.challenge = Challenge()
+        self.cipher = Cipher()
+
+
+        # Tkinter mainloop
+        self.root.mainloop()
+
+    # updates the gui timer
+    def update_timer(self):
+        self.timer.configure(text=self.game_time.get_time_left())
+        self.root.after(100, self.update_timer)
+
+    def update_challenge():
+        pass
+
+
+    # sets up GUI for application
+    def set_up_gui(self):
+        self.widget_w = int(self.WIDTH/80)
+        self.widget_h = int(self.HEIGHT/130)
+
+        self.set_up_code_box(1, 0)
+        self.set_up_timer_box(0, 0)
+        self.set_up_image_box(0, 1)
+        self.set_up_hints_box(1, 1)
+
+    # set up code box
+    def set_up_code_box(self, r, c):
+        self.code = Label(self.root, text="DF%$JD!WEK]", bg="black", fg="white",
+                            width=self.widget_w, height=self.widget_h, font=("Courier", 50))
+        self.code.grid(row=r, column=c)
+
+    # set up timer box
+    def set_up_timer_box(self, r, c):
+        self.timer = Label(self.root, text="This is where the timer will be", bg="black", fg="white",
+                            width=self.widget_w, height=self.widget_h, font=("Courier", 50))
+        self.timer.grid(row=r, column=c)
+
+    # set up image box
+    def set_up_image_box(self, r, c):
+        photo = PhotoImage(file="test.gif")
+
+        self.image = Label(self.root, image=photo, width=640, height=360, bg="black")
+        self.image.image = photo
+        self.image.grid(row=r, column=c)
+
+    # set up hints box
+    def set_up_hints_box(self, r, c):
+        self.hints = Label(self.root, text="This is where the hints will be\n I think", bg="black", fg="white",
+                            width=int(self.widget_w * (2.5)), height=int(self.widget_h * (2.6)), font=("Courier", 20))
+        self.hints.grid(row=r, column=c)
+
+
+# creates main challenge object
+class Challenge():
+        def __init__(self):
+                self.message = "Hello Everyone"
+                #modifies message to criteria
+                self.encode_code = cipher.modify_code("A")
+                # encypts message
+                self.encoded_list = cipher.encode(self.message, self.encode_code)
+                # gpio with corresponding letter
+                self.letters = {
+                        3:      "A",
+                        5:      "B",
+                        8:      "C",
+                        7:      "D",
+                        10:     "E",
+                        12:     "F",
+                        11:     "G",
+                        13:     "H",
+                        15:     "I",
+                        16:     "J",
+                        18:     "K",
+                        19:     "L",
+                        22:     "M",
+                        21:     "N",
+                        24:     "O",
+                        23:     "P",
+                        26:     "Q",
+                        29:     "R",
+                        32:     "S",
+                        31:     "T",
+                        33:     "U",
+                        36:     "V",
+                        35:     "W",
+                        38:     "X",
+                        37:     "Y",
+                        40:     "Z"
+                }
+
+                self.pins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36,37, 38, 40]
+                # set up GPIO
+                self.setup_gpio()
+
+        # sets up GPIO inputs for game
+        def setup_gpio(self):
+                GPIO.setmode(GPIO.BOARD)
+                for pin in self.letters.keys():
+                        GPIO.setup(self.pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+        # returns a list of letters for each grounded pin
+        def get_pins(self):
+                on_pins = []
+                for pin in self.letters.keys():
+                        input_state = GPIO.input(pin)
+                        if input_state == False:
+                                on_pins.append(self.letters[pin])
+                return on_pins
+
+        # return true or false depending on if user has solved message
+        def check_solved(self, message, decode_string):
+                if message == decode_string:
+                        return True
+                return False
+
+        def get_decrypt(self):
+                decode_guess = self.get_pins()
+                decode_code = cipher.modify_code(decode_guess)
+                decode_string = cipher.decode(self.encoded_list, decode_code)
+                print "Decrypted string: ", decode_string
+                print decode_guess
+
+                return decode_string
+
+
+    # holds all functions related to cipher
+    class Cipher():
 
         # modifies string value to be only unique characters and all uppercase
         def modify_code(self, string):
@@ -65,107 +214,34 @@ class Cipher():
                 return self.int_to_string(encoded_list)
 
 
+# creates a timer object
+class Timer(object):
+    def __init__(self, total_time):
+        self.total_time = total_time
+        self.start_time = time()
 
-class Game():
-        def __init__(self):
-                self.message = "Hello Everyone"
-                #modifies message to criteria
-                self.encode_code = cipher.modify_code("A")
-                # encypts message
-                self.encoded_list = cipher.encode(self.message, self.encode_code)
-                # gpio with corresponding letter
-                self.letters = {
-                        3:      "A",
-                        5:      "B",
-                        8:      "C",
-                        7:      "D",
-                        10:     "E",
-                        12:     "F",
-                        11:     "G",
-                        13:     "H",
-                        15:     "I",
-                        16:     "J",
-                        18:     "K",
-                        19:     "L",
-                        22:     "M",
-                        21:     "N",
-                        24:     "O",
-                        23:     "P",
-                        26:     "Q",
-                        29:     "R",
-                        32:     "S",
-                        31:     "T",
-                        33:     "U",
-                        36:     "V",
-                        35:     "W",
-                        38:     "X",
-                        37:     "Y",
-                        40:     "Z"
-                }
-                
-                self.pins = [3, 5, 7, 8, 10, 11, 12, 13, 15, 16, 18, 19, 21, 22, 23, 24, 26, 29, 31, 32, 33, 35, 36,37, 38, 40]
-                # set up GPIO
-                self.setup_gpio()
-
-        # sets up GPIO inputs for game
-        def setup_gpio(self):
-                GPIO.setmode(GPIO.BOARD)
-                for pin in self.letters.keys():
-                        GPIO.setup(self.pins, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-        # returns a list of letters for each grounded pin
-        def get_pins(self):
-                on_pins = []
-                for pin in self.letters.keys():
-                        input_state = GPIO.input(pin)
-                        if input_state == False:
-                                on_pins.append(self.letters[pin])
-                return on_pins
-        
-        # return true or false depending on if user has solved message
-        def check_solved(self, message, decode_string):
-                if message == decode_string:
-                        return True
-                return False
-
-        def get_decrypt(self):
-                decode_guess = self.get_pins()
-                decode_code = cipher.modify_code(decode_guess)
-                decode_string = cipher.decode(self.encoded_list, decode_code)
-                print "Decrypted string: ", decode_string
-                print decode_guess
-                
-                return decode_string
-                
-        
-
-        
+    def get_time_left(self):
+        current_time_left = self.total_time - time() + self.start_time
+        if current_time_left > 0:
+            return str(timedelta(seconds=current_time_left+1))[3:7]
+        return '0:00'
 
 
 # ------------ Encrypt --------------
-cipher = Cipher()
-game = Game()
-solved = False
+# cipher = Cipher()
+# game = Game()
+# solved = False
+#
+# try:
+#         while not solved:
+#                 decode_sting = game.get_decrypt()
+#                 solved = game.check_solved(game.message, decode_sting)
+#                 time.sleep(.5)
+
+
+
 
 try:
-        while not solved:
-                decode_sting = game.get_decrypt()
-                solved = game.check_solved(game.message, decode_sting)
-                time.sleep(.5)
-
-        GPIO.cleanup()
-        
+    app = App()
 except KeyboardInterrupt:
         GPIO.cleanup()
-
-
-
-
-
-
-
-
-
-
-
-
--- test files/windowtest.py
+++ test files/windowtest.py
@@ -18,42 +18,53 @@ class Timer(object):
             return str(timedelta(seconds=current_time_left+1))[3:7]
         return '0:00'
 
-class Window:
+class App:
     def __init__(self):
+        # gui set-up
+        self.WIDTH = 1280
+        self.HEIGHT = 720
         self.root = Tk()
         self.root.title("Constellation Crack")
         self.root.configure(bg="black")
-        self.game_time = Timer(300)
+        self.root.geometry('{}x{}'.format(self.WIDTH, self.HEIGHT))
         self.set_up_gui()
 
+        # create timer object
+        self.game_time = Timer(300)
         self.update_timer()
+
+
+        # Tkinter mainloop
         self.root.mainloop()
 
+    # updates the gui timer
     def update_timer(self):
         self.timer.configure(text=self.game_time.get_time_left())
         self.root.after(100, self.update_timer)
 
+    def update_code(self):
+        pass
 
 
     # sets up GUI for application
     def set_up_gui(self):
-        self.widget_w = int(WIDTH/90)
-        self.widget_h = int(HEIGHT/130)
+        self.widget_w = int(self.WIDTH/80)
+        self.widget_h = int(self.HEIGHT/130)
 
-        self.set_up_code_box(0, 0)
-        self.set_up_timer_box(1, 0)
+        self.set_up_code_box(1, 0)
+        self.set_up_timer_box(0, 0)
         self.set_up_image_box(0, 1)
         self.set_up_hints_box(1, 1)
 
     # set up code box
     def set_up_code_box(self, r, c):
-        self.code = Label(self.root, text="DF%$JD!WEK]", bg="green",
+        self.code = Label(self.root, text="DF%$J-D!WEK]", bg="black", fg="white",
                             width=self.widget_w, height=self.widget_h, font=("Courier", 50))
         self.code.grid(row=r, column=c)
 
     # set up timer box
     def set_up_timer_box(self, r, c):
-        self.timer = Label(self.root, text="This is where the timer will be", bg="red",
+        self.timer = Label(self.root, text="This is where the timer will be", bg="black", fg="white",
                             width=self.widget_w, height=self.widget_h, font=("Courier", 50))
         self.timer.grid(row=r, column=c)
 
@@ -61,18 +72,15 @@ class Window:
     def set_up_image_box(self, r, c):
         photo = PhotoImage(file="test.gif")
 
-        self.image = Label(self.root, image=photo,
-                            width=500, height=370, bg="black")
+        self.image = Label(self.root, image=photo, width=610, height=360, bg="black")
         self.image.image = photo
         self.image.grid(row=r, column=c)
 
     # set up hints box
     def set_up_hints_box(self, r, c):
-        self.hints = Label(self.root, text="This is where the hints will be", bg="orange",
-                            width=int(self.widget_w * (5/2.)), height=int(self.widget_h * (5/2.)), font=("Courier", 20))
+        self.hints = Label(self.root, text="This is where the hints will be\n I think", bg="black", fg="white",
+                            width=int(self.widget_w * (2.5)), height=int(self.widget_h * (2.6)), font=("Courier", 20))
         self.hints.grid(row=r, column=c)
 
-WIDTH = 1280
-HEIGHT = 720
 
-app = Window()
+app = App()
