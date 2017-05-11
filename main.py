fd6c83be91c8262d5f1e50a295ee140d1b012805
DJSanti <samantha.e.santiago@gmail.com>
2017-05-11 16:50:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 ############################
 # Names: Brady Anderson, Zach Brasseaux, Samantha Santiago
-# Date: 4/22/2017
+# Date: 5/11/2017
 # Objective: Create a cipher puzzle for the Raspberry Pi. Whoever
 # is solving has a certain amount of time to solve a puzzle
 # to get the correct name of a constellation.
+# THIS IS THE MOST RECENT VERSION OF THE SOURCE CODE
 ############################
 import RPi.GPIO as GPIO
 from Tkinter import *
